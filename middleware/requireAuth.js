8ea08348f2a686a97c305f5a4c320c1a60e0fe3d
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-10 10:23:36 -0400

    created the middleware for auth

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,7 +1,53 @@
+const ThingsService = require('../src/things/things-service');
+
 function requireAuth(req, res, next){
-    const token = req.header('Authorization');
-    console.log(token);
-    console.log(`Entered the Authentication middleware`);
+    const authToken = req.get('Authorization') || '';
+    let basicToken;
+
+    if(!authToken.toLowerCase().startsWith(`basic `)){
+        return res
+            .status(401)
+            .json({
+                error: {
+                    message: `Missing basic token`
+                }
+            })
+    }else{
+        basicToken = authToken.slice(`basic `.length, authToken.length);
+    }
+    
+    const [tokenUsername, tokenPassword] = Buffer
+        .from(basicToken, 'base64')
+        .toString()
+        .split(':');
+
+    if(!tokenUsername || !tokenPassword){
+        return res
+            .status(401)
+            .json({
+                error: {
+                    message: 'Unauthorized request'
+                }
+            })
+    }
+
+    req.app.get('db')('thingful_users')
+        .where({ user_name: tokenUsername })
+        .first()
+        .then((user) => {
+            if(!user || user.password !== tokenPassword){
+                return res
+                    .status(401)
+                    .json({
+                        error: {
+                            message: 'Unauthorized request'
+                        }
+                    })
+            }
+            req.user = user;
+            next();
+        })
+        .catch(next);
 }
 
 module.exports = {
